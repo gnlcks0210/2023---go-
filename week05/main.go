53b31a4e1112c296a17fcd1b639144065780ce04
gnlcks0210 <gnlcks0210@gmail.com>
2023-09-25 15:02:43 +0900

    week05 v0.7

--- week05/main.go
+++ week05/main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Print("Input score: ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err := reader.ReadString('\n') //option2
-	if err != nill {
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(inputScore)
