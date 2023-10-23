3d07c81510b1d33e74337059c9c7b5a1372ecbcb
gnlcks0210 <gnlcks0210@gmail.com>
2023-10-23 15:33:45 +0900

    week09v1.9

--- week05/main.go
+++ week05/main.go
@@ -4,27 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Print("Input score: ")
+	rand.Seed(time.Now().Unix()) // get the current data and time as an integer
+	answer := rand.Intn(100) + 1 // random integer number 1 ~ 100
+	fmt.Println("Guess Number Game~")
+	fmt.Println(answer)
+
 	reader := bufio.NewReader(os.Stdin)
-	inputScoreString, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("You have", 10-i, "chances")
+		fmt.Print("Input guess number:")
+		inputNumberString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputNumberString = strings.TrimSpace(inputNumberString)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputNumber == answer {
+			fmt.Println("Great! You got the number.") // Answer is higher
+			break
+		} else if inputNumber < answer {
+			fmt.Println("Guess number is lower then answer") // Answer is higher
+		} else if inputNumber > answer {
+			fmt.Println("Guess number is higher then answer") // Answer is lower
+		}
 	}
-	inputScoreString = strings.TrimSpace(inputScoreString) // remove space bar
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if inputScore >= 90 {
-		grade := "A grade!"
-	} else {
-		gread := "BCDE grad~"
-	}
-	fmt.Println("You got" + grade)
 }
---
+++ week09/main.go
@@ -0,0 +1,105 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	var b int = 20
+	var pa *int = &a
+
+	fmt.Printf("%T %T\n", &a, pa)
+	fmt.Printf("%x %x %x\n", &a, pa, &pa)
+	fmt.Println(&a, pa, &pa)
+	fmt.Println(*pa)
+	pa = &b
+	fmt.Println(*pa)
+}
+
+// package main
+
+// import "fmt"
+
+// func swap(n1 *int, n2 *int) {
+// 	fmt.Println(n1, n2)
+// 	temp := *n1
+// 	*n1 = *n2
+// 	*n2 = temp
+// }
+// func main() {
+// 	a := 10
+// 	b := 20
+
+// 	c := &a //var c *int = &a
+// 	fmt.Printf("%T\n", c)
+// 	fmt.Println(a, b)
+// 	swap(&a, &b) //pass by pointer
+// 	fmt.Println(a, b)
+// }
+
+// package main
+
+// import "fmt"
+
+// func swap(n1 *int, n2 *int) {
+// 	temp := *n1
+// 	*n1 = *n2
+// 	*n2 = temp
+// }
+// func main() {
+// 	a := 10
+// 	b := 20
+
+// 	c := &a //var c *int = &a
+// 	fmt.Printf("%T\n", c)
+// 	fmt.Println(a, b)
+// 	swap(&a, &b) //pass by pointer
+// 	fmt.Println(a, b)
+// }
+
+// package main
+
+// import "fmt"
+
+// func swap(n1 int, n2 int) {
+// 	temp := n1
+// 	n1 = n2
+// 	n2 = temp
+// }
+// func main() {
+// 	a := 10
+// 	b := 20
+
+// 	c := &a //var c *int = &a
+// 	fmt.Printf("%T\n", c)
+// 	fmt.Println(a, b)
+// 	swap(a, b)
+// 	fmt.Println(a, b)
+// }
+
+// package main
+
+// import "fmt"
+
+// func double(n *int) {
+// 	*n = *n * 2
+// }
+
+// func main() {
+// 	var a int = 6
+// 	double(&a)
+// 	fmt.Println(a)
+// }
+
+// package main
+
+// import "fmt"
+
+// func double(n int) {
+// 	n = n * 2
+// }
+
+// func main() {
+// 	var a int = 6
+// 	double(a)
+// 	fmt.Println(a)
+// }
