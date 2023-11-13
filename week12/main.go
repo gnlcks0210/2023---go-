8c7346f864db7951eeb03fd874023db11653ad52
gnlcks0210 <gnlcks0210@gmail.com>
2023-11-13 14:59:42 +0900

    week12 v3.3

--- week12/main.go
+++ week12/main.go
@@ -5,27 +5,14 @@ import (
 )
 
 func main() {
-	s := []int{0, 0, 0, 0, 0} //슬라이스 리터럴 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2]
 
-	s[4] = 99
-	s[2] = 91
+	as[1] = "z"
+	fmt.Println(a)
+	fmt.Println(as)
 
-	for _, value := range s {
-		fmt.Println(value)
-	}
-
-	copyS := s[1:4]
-
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"inha", "go", "student"} //배열 리터럴을 이용해서 test 배열 생성
-	//tests := test[0:4]//invalid argument: index 4 out of bounds [0:4]
-	tests := test[:2] //tests := test[0:2]
-	tests2 := test[1:]
-	tests2[0] = "python"
-	fmt.Println(tests2)
-	fmt.Println(tests, len(tests))
-	fmt.Println(test)
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
