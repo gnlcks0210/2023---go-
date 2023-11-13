e9b9e550a8a4c3fd5b37b554b6ede2116ffdec7f
gnlcks0210 <gnlcks0210@gmail.com>
2023-11-13 15:31:10 +0900

    week12 v3.6

--- week12/main.go
+++ week12/main.go
@@ -13,5 +13,7 @@ func main() {
 	c := append(a, "y", "x") // capacity가 바뀐다. 5 -> 10
 	fmt.Println(a, len(a), cap(a))
 	fmt.Println(c, len(c), cap(c))
-
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
+	c[0] = "k"
+	fmt.Println(a, c)
 }
