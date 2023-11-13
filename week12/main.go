d88b1a483e751f4c45f9e46a4b5f8709567de0c2
gnlcks0210 <gnlcks0210@gmail.com>
2023-11-13 14:23:11 +0900

    week12 v2.9

--- week12/main.go
+++ week12/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -10,6 +12,9 @@ func main() {
 	//s := make([]int, 5) make함수를 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
 
 	s := []int{0, 0, 0, 0, 0} //슬라이스 리터럴 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+
+	s[4] = 99
+	s[2] = 91
 
 	for _, value := range s {
 		fmt.Println(value)
